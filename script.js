4bf3aac221744d0e998ec68a5a55c7123d2c7ca3
isetkimo <80080259+isetkimo@users.noreply.github.com>
2026-07-06 19:56:33 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const items = document.querySelectorAll('.reveal');
-const observer = new IntersectionObserver((entries)=>{
-  entries.forEach(entry=>{
-    if(entry.isIntersecting){
-      entry.target.classList.add('show');
-    }
-  });
-},{threshold:0.12});
-items.forEach(item=>observer.observe(item));

cbb8de82ad8038730da8efba8622e9c5a79de20b
isetkimo <80080259+isetkimo@users.noreply.github.com>
2026-07-06 19:46:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const items = document.querySelectorAll('.reveal');
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+  });
+},{threshold:0.12});
+items.forEach(item=>observer.observe(item));
